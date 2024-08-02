56b5fd06384962ba1db13b25d328ae212ebf2b0c
imtathil <imtathilsetup@gmail.com>
2024-08-02 15:28:27 +0530

    Update settings.py

--- web_project/settings.py
+++ web_project/settings.py
@@ -277,7 +277,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 #making satic folder for all the project
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 #STATICFILES_DIRS = os.path.join(BASE_DIR,"staticfiles")
 #STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles_build","static")
 #STATICFILES_DIRS =[
